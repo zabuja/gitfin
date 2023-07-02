4545ea332dd7179d9c171fc959f94f32b42236eb
atanu <atanu.tmsl@gmail.com>
2023-07-02 11:33:38 +0530

    added loader

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,21 +1,43 @@
 import React from "react";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import Spinner from "../layout/Spinner";
+
 const UserResults = () => {
+
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
     fetchUsers()
   }, []);
 
   const fetchUsers = async () => {
-    const response = await fetch(`${import.meta.env.VITE_GITHUB_URL}`, {
+    const response = await fetch(`${import.meta.env.VITE_GITHUB_URL}users`, {
       headers:{
         Authorization: `Bearer ${import.meta.env.VITE_GITHUB_TOKEN}`
       }
     })
     const data = await response.json()
-    console.log(data)
+    setUsers(data)
+    setLoading(false)
   }
 
-  return <div>UserResults</div>
+
+  if(!loading){
+    return(
+      <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
+        {users.map((user) => (
+          <h3>{user.login}</h3>
+        ))}
+      </div>
+    )
+  }
+  else{
+    return(
+      <Spinner/>
+    )
+  }
+  
 }
 
 export default UserResults;
