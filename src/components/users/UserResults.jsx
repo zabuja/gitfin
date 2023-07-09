f284e1bb2c20d5155fb88fc6309b5253f6d554fe
atanu <atanu.tmsl@gmail.com>
2023-07-10 00:37:19 +0530

    implemented the search bar and clear button; need to implement the search logic

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -13,9 +13,6 @@ const UserResults = () => {
 
 
 
-
- 
-
 console.log(users)
   if(!loading){
     return(
--- src/components/users/UserSearch.jsx
+++ src/components/users/UserSearch.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import React from "react";
+import { useState, useContext } from "react";
+import GithubContext from "../../context/github/GithubContext";
 
 const UserSearch = () => {
-  return (
-    <div>UserSearch</div>
-  )
-}
+  const [text, setText] = useState("");
 
-export default UserSearch
+  const {users} = useContext(GithubContext)
+
+  const handleChange = (e) => {
+    setText(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (text === "") {
+      alert("please enter something");
+    } else {
+      // todo -search users
+      setText("");
+    }
+  };
+
+  return (
+    <div className="grid grid-cols-1 xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-2 mb-8 gap-8 ">
+      <div>
+        <form onSubmit={handleSubmit}>
+          <div className="form-control">
+            <div className="relative">
+              <input
+                type="text"
+                className="w-full pr-40 bg-gray-200 input input-lg text-black"
+                placeholder="Search"
+                value={text}
+                onChange={handleChange}
+              />
+              <button className="absolute top-0 right-0 rounded-l-none w-36 btn btn-lg hover:bg-purple-500 text-white">
+                Go
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+
+      {users.length >0 && (
+        <div>
+        <button className="btn-ghost btn-lg  rounded-[20px] hover:bg-purple-500 text-white ">
+          Clear
+        </button>
+      </div>
+      )}
+    </div>
+  );
+};
+
+export default UserSearch;
